52b18aa1dde34c9b45441f8f62f3889a26a07178
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-11-04 22:18:05 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

---
+++ ADS2- assignment/m2/percolation/DepthFirstPaths.java
@@ -0,0 +1,75 @@
+/**
+ *class for depth first paths.
+ */
+public class DepthFirstPaths {
+    /**
+     *the array to mark the visited.
+     *vertex
+     */
+    private boolean[] marked;
+    /**
+     *the array to maintian the.
+     *relation path.
+     */
+    private int[] edgeTo;
+    /**
+     *the variable to maintain source vertex.
+     */
+    private final int s;
+    /**
+     * Computes a path between s and.
+     * every other vertex in graph
+     * @param graph the graph
+     * @param st the source vertex
+     */
+    public DepthFirstPaths(final Graph graph,
+    final int st) {
+        this.s = st;
+        edgeTo = new int[graph.vertices()];
+        marked = new boolean[graph.vertices()];
+        validateVertex(s);
+        dfs(graph, s);
+    }
+
+    /**
+     *the method is to perform the depth.
+     *first search.
+     *time complexity is O(E)
+     *E is the edges.
+     * @param      graph     { parameter_description }
+     * @param      v     { parameter_description }
+     */
+    private void dfs(final Graph graph, final int v) {
+        marked[v] = true;
+        for (int w : graph.adj(v)) {
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                dfs(graph, w);
+            }
+        }
+    }
+
+    /**
+     * Is there a path between the source.
+     * vertex { s} and vertex {v}?
+     * @param v the vertex
+     * @return true if there is a path,
+     * false} otherwise
+     */
+    public boolean hasPathTo(final int v) {
+        validateVertex(v);
+        return marked[v];
+    }
+
+    /**
+     *throw an IllegalArgumentException unless.
+     *@param v is the vertex
+     */
+    private void validateVertex(final int v) {
+        int len = marked.length;
+        if (v < 0 || v >= len) {
+            throw new IllegalArgumentException(
+                "vertex " + v + " is not between 0 and " + (len - 1));
+        }
+    }
+}
---
+++ ADS2- assignment/m2/percolation/Graph.java
@@ -0,0 +1,159 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+/**
+ *the class graph is to construct a graph.
+ */
+public class Graph {
+    /**
+     *the variable to maintain vertex.
+     */
+    private final int vertices;
+    /**
+     *the variable to maintain edges.
+     */
+    private int edges;
+    /**
+     *the variable to adjacency matrix.
+     */
+    private boolean[][] adj;
+    /**
+     *the constructor to initialize.
+     *
+     * @param      vertex  vertices
+     */
+    public Graph(final int vertex) {
+        this.vertices = vertex;
+        this.edges = 0;
+        this.adj = new boolean[vertices][vertices];
+    }
+    /**
+     *
+     *the method to return vertices.
+     * @return vertices.
+     */
+    public int vertices() {
+        return vertices;
+    }
+    /**
+     *the method is to return edges.
+     *
+     * @return  edges.
+     */
+    public int edges() {
+     return edges;
+ }
+ /**
+  * adds an edge.
+  *
+  * @param      v  vertexOne
+  * @param      w  vertexTwo
+  */
+    public void addEdge(final int v,
+    final int w) {
+        if (!adj[v][w]) {
+            edges++;
+        }
+        adj[v][w] = true;
+        adj[w][v] = true;
+    }
+    /**
+     *the method returns whether there is.
+     *a connection between two vertices.
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return connection is there are not.
+     */
+    public boolean contains(final int v,
+    final int w) {
+        return adj[v][w];
+    }
+    /**
+     * return list of neighbors of v.
+     *
+     * @param      v  vertex
+     * time complexity is O(N)
+     * N is the vertices.
+     * @return  iterator
+     */
+    public Iterable<Integer> adj(final int v) {
+        return new AdjIterator(v);
+    }
+    /**
+     *support iteration over graph vertices.
+     */
+    private class AdjIterator implements Iterator<Integer>,
+     Iterable<Integer> {
+        /**
+         *the variable to make vertex count.
+         */
+        private int v;
+        /**
+         *the variable to store another vertex.
+         */
+        private int w = 0;
+        /**
+         *the constructor to initialize.
+         *
+         * @param      vx vertex
+         */
+        AdjIterator(final int vx) {
+            this.v = vx;
+        }
+        /**
+         *an iterator method.
+         *
+         * @return an iterator.
+         */
+        public Iterator<Integer> iterator() {
+            return this;
+        }
+/**
+ * has next method to maitain the connection.
+ *
+ * @return     True if has next, False otherwise.
+ */
+        public boolean hasNext() {
+            while (w < vertices) {
+                if (adj[v][w]) {
+                    return true;
+                }
+                w++;
+            }
+            return false;
+        }
+        /**
+         * to go to next element.
+         *
+         * @return     { description_of_the_return_value }
+         */
+        public Integer next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return w++;
+        }
+
+        // public void remove()  {
+        //     throw new UnsupportedOperationException();
+        // }
+    }
+    /**
+     *string representation of Graph - takes quadratic time.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(vertices + " " + edges + "\n");
+        for (int v = 0; v < vertices; v++) {
+            s.append(v + ": ");
+            for (int w : adj(v)) {
+                s.append(w + " ");
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
+
+}

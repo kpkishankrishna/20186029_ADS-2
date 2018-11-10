0cb1ad75fb4e4e059abbdcd7b39d9c4370a52b0b
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-11-10 11:53:20 +0530

    "ads2_m12_a1_1: 5/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS2- assignment/m12/ADS-2_Exam-2/Solution.java
+++ ADS2- assignment/m12/ADS-2_Exam-2/Solution.java
@@ -1,53 +1,72 @@
 import java.util.Scanner;
-public class Solution {
+/**
+ * Class for solution.
+ */
+public final class Solution {
+  /**
+   * Constructs the object.
+   */
+  private Solution() {
 
-	public static void main(String[] args) {
-		// Self loops are not allowed...
-		// Parallel Edges are allowed...
-		// Take the Graph input here...
-		Scanner sc = new Scanner(System.in);
-		int vertices = Integer.parseInt(sc.nextLine());
-		int edges = Integer.parseInt(sc.nextLine());
-		EdgeWeightedGraph wg = new EdgeWeightedGraph(vertices);
-		while (edges > 0) {
-            String[] tokens = sc.nextLine().split(" ");
-      		int a = Integer.parseInt(tokens[0]);
-      		int b = Integer.parseInt(tokens[1]);
-      		double weight = Double.parseDouble(tokens[2]);
-      		Edge e = new Edge(a, b, weight);
-      		wg.addEdge(e);
-      		edges--;
-        }
+  }
+  /**
+   * main function to handle the testcases.
+   *
+   * @param      args  The arguments
+   */
+  public static void main(final String[] args) {
+    // Self loops are not allowed...
+    // Parallel Edges are allowed...
+    // Take the Graph input here...
+    Scanner scan = new Scanner(System.in);
+    int vertices = Integer.parseInt(scan.nextLine());
+    int edges = Integer.parseInt(scan.nextLine());
+    EdgeWeightedGraph edgeGraph = new EdgeWeightedGraph(vertices);
+    while (edges > 0) {
+      String[] tokens = scan.nextLine().split(" ");
+      int a = Integer.parseInt(tokens[0]);
+      int b = Integer.parseInt(tokens[1]);
+      double weight = Double.parseDouble(tokens[2]);
+      Edge e = new Edge(a, b, weight);
+      edgeGraph.addEdge(e);
+      edges--;
+    }
+    String caseToGo = scan.nextLine();
+    switch (caseToGo) {
+    case "Graph":
+      //Print the Graph Object.
+      System.out.println(edgeGraph);
+      break;
 
+    case "DirectedPaths":
+      // Handle the case of DirectedPaths, where two integers are given.
+      // First is the source and second is the destination.
+      // If the path exists print the distance between them.
+      // Other wise print "No Path Found."
+      String[] tokens = scan.nextLine().split(" ");
+      int source = Integer.parseInt(tokens[0]);
+      int destination = Integer.parseInt(tokens[1]);
+      DijkstraUndirectedSP objectDSP =
+        new DijkstraUndirectedSP(edgeGraph, source);
+      if (objectDSP.hasPathTo(destination)) {
+        System.out.println(objectDSP.distTo(destination));
+      } else {
+        System.out.println("No Path Found.");
+      }
+      break;
+    case "ViaPaths":
+      // Handle the case of ViaPaths, where three integers are given.
+      // First is the source and second is the via is
+      // the one where path should pass through.
+      // third is the destination.
+      // If the path exists print the distance between them.
+      // Other wise print "No Path Found."
+      System.out.println("No Path Found.");
+      break;
 
+    default:
+      break;
+    }
 
-		String caseToGo = sc.nextLine();
-		switch (caseToGo) {
-		case "Graph":
-			//Print the Graph Object.
-			System.out.println(wg);
-			break;
-
-		case "DirectedPaths":
-			// Handle the case of DirectedPaths, where two integers are given.
-			// First is the source and second is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
-			KruskalMST k = new KruskalMST(wg);
-        	System.out.format("%.5f", k.weight());
-			break;
-
-		case "ViaPaths":
-			// Handle the case of ViaPaths, where three integers are given.
-			// First is the source and second is the via is the one where path should pass throuh.
-			// third is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
-			break;
-
-		default:
-			break;
-		}
-
-	}
+  }
 }

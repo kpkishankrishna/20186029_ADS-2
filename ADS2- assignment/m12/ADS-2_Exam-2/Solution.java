13ab2a803b6b10693673beac9b1e3f68142aa56b
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-11-10 12:00:52 +0530

    "ads2_m12_a1_1: 5/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS2- assignment/m12/ADS-2_Exam-2/Solution.java
+++ ADS2- assignment/m12/ADS-2_Exam-2/Solution.java
@@ -18,24 +18,24 @@ public final class Solution {
     // Self loops are not allowed...
     // Parallel Edges are allowed...
     // Take the Graph input here...
-    Scanner scan = new Scanner(System.in);
-    int vertices = Integer.parseInt(scan.nextLine());
-    int edges = Integer.parseInt(scan.nextLine());
-    EdgeWeightedGraph edgeGraph = new EdgeWeightedGraph(vertices);
+    Scanner sc = new Scanner(System.in);
+    int vertices = Integer.parseInt(sc.nextLine());
+    int edges = Integer.parseInt(sc.nextLine());
+    EdgeWeightedGraph ewg = new EdgeWeightedGraph(vertices);
     while (edges > 0) {
-      String[] tokens = scan.nextLine().split(" ");
-      int a = Integer.parseInt(tokens[0]);
-      int b = Integer.parseInt(tokens[1]);
+      String[] tokens = sc.nextLine().split(" ");
+      int c = Integer.parseInt(tokens[0]);
+      int r = Integer.parseInt(tokens[1]);
       double weight = Double.parseDouble(tokens[2]);
-      Edge e = new Edge(a, b, weight);
-      edgeGraph.addEdge(e);
+      Edge e = new Edge(c, r, weight);
+      ewg.addEdge(e);
       edges--;
     }
-    String caseToGo = scan.nextLine();
+    String caseToGo = sc.nextLine();
     switch (caseToGo) {
     case "Graph":
       //Print the Graph Object.
-      System.out.println(edgeGraph);
+      System.out.println(ewg);
       break;
 
     case "DirectedPaths":
@@ -43,11 +43,11 @@ public final class Solution {
       // First is the source and second is the destination.
       // If the path exists print the distance between them.
       // Other wise print "No Path Found."
-      String[] tokens = scan.nextLine().split(" ");
+      String[] tokens = sc.nextLine().split(" ");
       int source = Integer.parseInt(tokens[0]);
       int destination = Integer.parseInt(tokens[1]);
       DijkstraUndirectedSP objectDSP =
-        new DijkstraUndirectedSP(edgeGraph, source);
+        new DijkstraUndirectedSP(ewg, source);
       if (objectDSP.hasPathTo(destination)) {
         System.out.println(objectDSP.distTo(destination));
       } else {
